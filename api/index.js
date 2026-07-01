ef3c5686dbd084ef5d0f11f177b1da28ab70ee1e
bopaso <bopaso@gmail.com>
2026-07-01 13:49:31 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const path = req.url === '/' ? '' : req.url;
   res.writeHead(301, {
-    Location: `https://do-ce.cl${path}`
+    Location: `https://do-ce.com${path}`
   });
   res.end();
 };
